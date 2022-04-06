f40817a1079c7acd8eab287a54a61839c4b27d53
brandonlee <brandonlee@localhost>
2022-04-06 13:11:00 -0700

    initial commit

---
+++ HCLRestApi/src/test/java/com/restservices/project/HclRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restservices.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HclRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
